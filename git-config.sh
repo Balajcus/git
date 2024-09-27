aaeb84ad6018874b8368502ebd7803131fa3d974
balajcus <s2023balajkamartin@skolabaltaci.cz>
2024-09-27 08:34:05 +0200

    repos

---
+++ git-config.sh
@@ -0,0 +1,6 @@
+#!bin/gash
+echo "Welcome to Git config"
+git config --global user.email "s2023balajkamartin@skolabaltaci.cz"
+git config --global user.name "balajcus"
+git config --list
+echo "Done succesfully"
